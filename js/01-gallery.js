7c197301b23d2e7e09c855a55566b387c03ece7a
Rogok-Serg <rogoksv@gmail.com>
2023-04-23 01:59:21 +0300

    Finish 7 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+const galleryRef = document.querySelector('.gallery');
+
+galleryItems.map(({ preview, original, description }) => {
+  const liEl = `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  galleryRef.innerHTML += liEl;
+})
+
+galleryRef.addEventListener('click', onClickImg);
+
+function onClickImg(event) {
+  event.preventDefault()
+  if (event.target.nodeName !== 'IMG') {
+    return
+  }
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+  instance.show();
+  galleryRef.addEventListener('keydown', onClickModalClose);
+  
+  function onClickModalClose(event) {
+    if (event.code === "Escape") {
+      instance.close()
+    } 
+  };
+};
+
+
+
+
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
 
-console.log(galleryItems);
+galleryItems.map(({ preview, original, description }) => {
+  const liEl = `<li class="gallery__item">
+   <a class="gallery__link" 
+      href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+  galleryRef.innerHTML += liEl;
+})
+const galleryLightBox = new SimpleLightbox('.gallery__link', {captionsData: "alt", captionsDelay: 250});
+
+console.log(galleryItems );

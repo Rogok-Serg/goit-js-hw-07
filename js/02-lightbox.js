7be9ced508202de76797d9c0c71ba617ae032fd4
Rogok-Serg <rogoksv@gmail.com>
2023-04-29 17:00:23 +0300

    correction task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,15 +2,15 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryRef = document.querySelector('.gallery');
 
-galleryItems.map(({ preview, original, description }) => {
-  const liEl = `<li class="gallery__item">
+const liEl = galleryItems.reduce((item, { preview, original, description }) => 
+  item + `<li class="gallery__item">
    <a class="gallery__link" 
       href="${original}">
       <img class="gallery__image" src="${preview}" alt="${description}" />
    </a>
 </li>`
-  galleryRef.innerHTML += liEl;
-})
+  , "");
+  galleryRef.insertAdjacentHTML("beforeend", liEl)
 const galleryLightBox = new SimpleLightbox('.gallery__link', {captionsData: "alt", captionsDelay: 250});
 
 console.log(galleryItems );
